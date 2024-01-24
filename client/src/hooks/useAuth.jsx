6275a756920392663cb798d90767af13f1e70fa4
thecodermehedi <thecodermehedi@gmail.com>
2024-01-24 11:21:26 +0600

    Add useAuth hook

---
+++ client/src/hooks/useAuth.jsx
@@ -0,0 +1,10 @@
+import { AuthContext } from "@/context/AuthContext";
+import { useContext } from "react";
+
+
+const useAuth = () => {
+  const auth = useContext(AuthContext);
+  return auth;
+};
+
+export default useAuth;
