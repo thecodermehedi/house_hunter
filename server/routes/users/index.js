5e7ee2475f037cc40a0aefb73ee73d6d75ea13b3
thecodermehedi <thecodermehedi@gmail.com>
2024-01-24 11:22:54 +0600

    Refactor user routes and controllers

--- server/routes/users/index.js
+++ server/routes/users/index.js
@@ -1,6 +1,10 @@
 const router = require("../../utils/router");
-const addUser = require("../../controllers/users/addUser");
+const RegisterUser = require("../../controllers/users/RegisterUser");
+const LoginUser = require("../../controllers/users/LoginUser");
+const logoutUser = require("../../controllers/users/LogoutUser");
 
-router.post("/api/v1/users", addUser);
+router.post("/api/v1/auth/register", RegisterUser);
+router.post("/api/v1/auth/login", LoginUser);
+router.get("/api/v1/auth/logout", logoutUser);
 
 module.exports = router;
