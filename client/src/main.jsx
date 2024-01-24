35402cc3f30c6963ec9de5d0f582379909353f80
thecodermehedi <thecodermehedi@gmail.com>
2024-01-24 11:20:57 +0600

    Add AuthProvider to main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,13 +5,16 @@ import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import {Toaster} from "react-hot-toast";
 import "./index.css";
 import router from "./routes";
+import {AuthProvider} from "./context/AuthContext";
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <HelmetProvider>
-    <QueryClientProvider client={queryClient}>
-      <Toaster />
-      <RouterProvider router={router} />
-    </QueryClientProvider>
-  </HelmetProvider>
+  <AuthProvider>
+    <HelmetProvider>
+      <QueryClientProvider client={queryClient}>
+        <Toaster />
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </HelmetProvider>
+  </AuthProvider>
 );
